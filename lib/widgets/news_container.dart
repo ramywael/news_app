103a5e2de844b6e912dec3f7c7bef4cb5bb4cc61
ramywael <ramybakry9@gmail.com>
2023-09-05 22:24:55 +0300

    Display newsData on newItemsListView

--- lib/widgets/news_container.dart
+++ lib/widgets/news_container.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/models/article_model.dart';
 
-class NewsTile extends StatelessWidget { 
-  const NewsTile({super.key});
+class NewsTile extends StatelessWidget {
+  final ArticalModel article;
+  const NewsTile({super.key, required this.article});
 
   @override
   Widget build(BuildContext context) {
@@ -17,33 +19,34 @@ class NewsTile extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            // child: Image.network(
-            //   'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-            //   height: 200,
-            //   width: MediaQuery.of(context).size.width,
-            //   fit: BoxFit.cover,
-            // ),
-           child: CachedNetworkImage(
-              imageUrl:
-                  'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-              errorWidget: (context, url, error) => const Icon(Icons.error,color: Colors.red,),
-              placeholder: (context, url) => const Center(
-                child: CircularProgressIndicator(),
-              ),
-            )
-          ),
+              borderRadius: BorderRadius.circular(6),
+              // child: Image.network(
+              //   'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+              //   height: 200,
+              //   width: MediaQuery.of(context).size.width,
+              //   fit: BoxFit.cover,
+              // ),
+              child: CachedNetworkImage(
+                imageUrl: "${article.urlToImage}",
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                errorWidget: (context, url, error) => const Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+                placeholder: (context, url) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )),
           const SizedBox(
             height: 12,
           ),
-          const Text(
-            'Large Title should be places in this place Large Title should be places in this place sdfadsf',
+          Text(
+            article.title,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.black87,
               fontSize: 20,
               fontWeight: FontWeight.w500,
@@ -52,10 +55,10 @@ class NewsTile extends StatelessWidget {
           const SizedBox(
             height: 4,
           ),
-          const Text(
-            'and here is the description of the news you can place your desc here',
+          Text(
+            article.description,
             maxLines: 2,
-            style: TextStyle(color: Colors.black54, fontSize: 14),
+            style: const TextStyle(color: Colors.black54, fontSize: 14),
           )
         ],
       ),
--- lib/widgets/news_item_list_view.dart
+++ lib/widgets/news_item_list_view.dart
@@ -1,26 +1,54 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/models/article_model.dart';
+import 'package:news_app_ui_setup/services/get_news_api.dart';
 
 import 'news_container.dart';
 
-class NewsItemListView extends StatelessWidget {
+class NewsItemListView extends StatefulWidget {
   const NewsItemListView({
     super.key,
   });
 
   @override
+  State<NewsItemListView> createState() => _NewsItemListViewState();
+}
+
+class _NewsItemListViewState extends State<NewsItemListView> {
+  List<ArticalModel> articles = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getGeneralNews(); //It will be called only once
+  }
+
+  void getGeneralNews() async {
+    articles = await Service(Dio()).getNews();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  SliverList(
-      //this solution is better than the above one
-      //Because this one is more efficient for performance
-      delegate: SliverChildBuilderDelegate(
-        childCount: 10,
-            (BuildContext context, int index) {
-          return const Padding(
-            padding: EdgeInsets.only(bottom: 16),
-            child: NewsTile(),
+    return isLoading
+        ? const SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()))
+        : SliverList(
+            //this solution is better than the above one
+            //Because this one is more efficient for performance
+            delegate: SliverChildBuilderDelegate(
+              childCount: articles.length,
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: NewsTile(
+                    article: articles[index],
+                  ),
+                );
+              },
+            ),
           );
-        },
-      ),
-    );
   }
 }
